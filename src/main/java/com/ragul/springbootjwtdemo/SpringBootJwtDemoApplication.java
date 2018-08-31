9122b45d4784ac9daa4ff20e2bb4279363d099fb
Ragulan <Ragulan28@gmail.com>
2018-08-31 15:12:16 +0530

    init

---
+++ src/main/java/com/ragul/springbootjwtdemo/SpringBootJwtDemoApplication.java
@@ -0,0 +1,12 @@
+package com.ragul.springbootjwtdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtDemoApplication.class, args);
+    }
+}
